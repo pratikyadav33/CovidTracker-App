1a3e4e4bc558b7c78db6409787a18d81267663a6
Pratik <Pratikyadav33@gmail.com>
2021-04-10 06:38:15 +0530

    Main_activity design Completed

---
+++ app/src/main/java/com/example/covidtracker/CountryData.kt
@@ -0,0 +1,5 @@
+package com.example.covidtracker
+
+class CountryData {
+
+}
--- app/src/main/java/com/example/covidtracker/MainActivity.kt
+++ app/src/main/java/com/example/covidtracker/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.covidtracker
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    val flag: ImageView = findViewById(R.id.flag)
+    val spinner : Spinner=findViewById(R.id.spinner)
+    val stat : Button = findViewById(R.id.stat)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
